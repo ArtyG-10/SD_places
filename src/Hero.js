16f54807409353a966573dc8e1dff06f4acbe6c5
Arturo Gourentchik <a.gourent@outlook.com>
2021-11-15 19:24:59 -0800

    assets folder and flexbox

--- src/Hero.js
+++ src/Hero.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import LaJolla from './lajolla.jpg';
-import starlite from './starlite.jpg';
+import LaJolla from './assets/lajolla.jpg';
+import starlite from './assets/starlite.jpg';
 
 class Hero extends React.Component {
   constructor(props) {
@@ -14,8 +14,10 @@ class Hero extends React.Component {
     return (
       <div className="hero">
         <h1>Explore All There Is...</h1>
+        <div className="heroImages">
         <img src={LaJolla}></img>
         <img src={starlite}></img>
+        </div>
       </div>
     )
   }
